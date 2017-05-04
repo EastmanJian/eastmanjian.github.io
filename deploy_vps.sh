65d478d618bd6984352efbd222a8ba823b6b37c6
Eastman Jian <50935@qq.com>
2017-05-05 02:56:31 +0800

    clean up

--- deploy_vps.sh
+++
@@ -1,7 +0,0 @@
-tar cvfz _site.tar.gz _site/
-scp -P 28411 _site.tar.gz root@45.78.51.215:/srv/www/htdocs
-ssh -p 28411 root@45.78.51.215 tar xvfz /srv/www/htdocs/_site.tar.gz
-ssh -p 28411 root@45.78.51.215 rm -rf /srv/www/htdocs/blog
-ssh -p 28411 root@45.78.51.215 mv _site /srv/www/htdocs/blog
-ssh -p 28411 root@45.78.51.215 rm -f /srv/www/htdocs/_site.tar.gz
-rm -f _site.tar.gz
